918299c8ade7bd704b5bd2d742aa5598a93f3b82
satvik <satvik.jain.kht@gmail.com>
2023-07-18 05:25:28 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from turtle import Turtle, Screen
+
+screen = Screen()
+screen.setup(width=500,height=400)
+user_bet = screen.textinput(title = "Make your bet", prompt="Which turtle will win? Enter your color: ")
+
+tim=Turtle()
+
+screen.colormode()
